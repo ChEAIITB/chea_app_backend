5c0267b9d4de2dbc597fff256f987c461aae88d7
ChEAIITB <148718560+ChEAIITB@users.noreply.github.com>
2026-09-03 14:41:56 +0530

    Update google.js

--- config/google.js
+++ config/google.js
@@ -1,24 +1,41 @@
 const { google } = require("googleapis");
-require("dotenv").config({
-    path:__dirname+"./../.env"
-});
-const fs = require("fs");
+
+const requiredEnv = [
+    "GOOGLE_TYPE",
+    "GOOGLE_PROJECT_ID",
+    "GOOGLE_PRIVATE_KEY_ID",
+    "GOOGLE_PRIVATE_KEY",
+    "GOOGLE_CLIENT_EMAIL",
+    "GOOGLE_CLIENT_ID",
+    "GOOGLE_AUTH_URI",
+    "GOOGLE_TOKEN_URI",
+    "GOOGLE_AUTH_PROVIDER_X509_CERT_URL",
+    "GOOGLE_CLIENT_X509_CERT_URL",
+    "GOOGLE_UNIVERSE_DOMAIN"
+];
+
+for (const key of requiredEnv) {
+    if (!process.env[key]) {
+        throw new Error(`Missing environment variable: ${key}`);
+    }
+}
+
 const googleCredentials = {
-  "type": process.env.GOOGLE_TYPE,
-  "project_id": process.env.GOOGLE_PROJECT_ID,
-  "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
-  "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  "client_email": process.env.GOOGLE_CLIENT_EMAIL,
-  "client_id": process.env.GOOGLE_CLIENT_ID,
-  "auth_uri": process.env.GOOGLE_AUTH_URI,
-  "token_uri": process.env.GOOGLE_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url": process.env.GOOGLE_CLIENT_X509_CERT_URL,
-  "universe_domain": process.env.GOOGLE_UNIVERSE_DOMAIN
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url:
+        process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url:
+        process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN
 };
-// const keyFile = JSON.parse(
-//     fs.readFileSync("./sheetKey.json", "utf8")
-// );
+
 const auth = new google.auth.GoogleAuth({
     credentials: googleCredentials,
     scopes: [
